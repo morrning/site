64ea7b8dff487b0fd6039cc66f204c39c2bb1feb
Babak Alizadeh <alizadeh.babak@gmail.com>
2020-04-21 13:02:21 +0430

    start project

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function homePage()
+    {
+        return $this->render('home/index.html.twig', [
+        ]);
+    }
+    /**
+     * @Route("/page/contact", name="pageContact")
+     */
+    public function pageContact()
+    {
+        return $this->render('home/contact.html.twig', [
+        ]);
+    }
+    /**
+     * @Route("/page/about", name="pageAbout")
+     */
+    public function pageAbout()
+    {
+        return $this->render('home/about.html.twig', [
+        ]);
+    }
+    /**
+     * @Route("/404", name="404")
+     */
+    public function error404()
+    {
+        return $this->render('home/404.html.twig', [
+        ]);
+    }
+}
